34856eba2497fb85bfe99d629b6cec3bdcf8e0f2
Alfonso Martinez <34066160+juniormartinez1995@users.noreply.github.com>
2018-12-08 19:30:19 -0300

    update
    few changes

--- FAT12reader.c
+++ FAT12reader.c
@@ -6,44 +6,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-
-struct {
-
-    unsigned char salto[3];
-
-    unsigned char nome_oem[8];
-
-    unsigned char bytes_por_setor[2];
-
-    unsigned char setores_por_cluster[1];
-
-    unsigned char setores_reservados[2];
-
-    unsigned char copias_da_fat[1];
-
-    unsigned char entradas_diretorio_raiz[2];
-
-    unsigned char numero_de_setores[2];
-
-    unsigned char descritor_de_midia[1];
-
-    unsigned char setores_por_copia_da_fat[2];
-
-    unsigned char setores_por_trilha[2];
-
-    unsigned char numero_de_cabecas[2];
-
-    unsigned char setores_ocultos[2];
-
-    unsigned char codigo_de_boot[480];
-
-    unsigned char assinatura_de_boot[2];
-
-} BOOT;
-
-
-
-struct {
+typedef struct descritor {
 
     char *nome_do_arquivo;
 
@@ -64,7 +27,6 @@ struct {
 } DESCRITOR;
 
 
-//removeSpaces
 void remove_espacos(char* k)
 {
 	char* i = k;
@@ -80,12 +42,12 @@ void remove_espacos(char* k)
 	*i = 0;
 }
 
-//getBytesPerSector
+//Pegar bytes por setor
 int bytes_por_setor(char* a)
 {
 	return (int) (a[11] | a[12] << 8);
 }
-//getFileAttribute
+//Pegar atributos do arquivo
 int atributos_dr(char* a, int endereco)
 {
 	int end_atributo = endereco + 11;
@@ -104,7 +66,8 @@ int atributos_dr(char* a, int endereco)
 		return 1;
 	}
 }
-//getFileSize
+
+//Pegar o tamanho do arquivo
 int tamanho_arq(char* a, int i)
 {
 	return (int)((a[i + 28] & 0xFF) 
@@ -112,7 +75,8 @@ int tamanho_arq(char* a, int i)
 		| ((a[i + 30] & 0xFF) << 16) 
 		| ((a[i + 31] & 0xFF) << 24));
 }
-//getFileName
+
+//Pegar o nome do arquivo
 char* nome_arq(char* a, int i)
 {
 	char* nomeArq = malloc(sizeof(char));
@@ -146,6 +110,7 @@ char* nome_arq(char* a, int i)
 int main(int argc, char* argv[])
 {
 	DESCRITOR ds;
+
     if (argc != 2)
     {
         printf("E necessario passar o arquivo imagem como argumento\n");
@@ -171,14 +136,14 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-	int bytesPerSector = bytes_por_setor(p);
-	int rootAddress = 19 * bytesPerSector;
-	int rootSize = 14 * bytesPerSector;
+	int bytes_setor = bytes_por_setor(p);
+	int end_raiz = 19 * bytes_setor;
+	int tam_raiz = 14 * bytes_setor;
 
 	/* Loop para printar todos os arquivos do diretório raiz. */
-	for (int i = rootAddress; i < rootAddress + rootSize; i += 32)
+	for (int i = end_raiz; i < end_raiz + tam_raiz; i += 32)
 	{
-		int ds.atributos = atributos_dr(p, i); //attribute = ds.atributo
+		ds.atributos = atributos_dr(p, i); 
 		if (ds.atributos != -1 && (int)p[i] != 0)
 		{
 			/* Pegar o tipo, tamanho do arquivo, e o seu nome*/
@@ -187,22 +152,22 @@ int main(int argc, char* argv[])
 			ds.nome_do_arquivo = nome_arq(p, i);
 
 			/* Pegar a hora de criação do arquivo */
-			int creationTime = (int)((p[i + 14] & 0xFF) | (p[i + 15] & 0xFF) << 8);
-			int hour = creationTime >> 11;
-			int minute = (creationTime >> 5) & 0x3F;
-			int second =  (creationTime & 0x1F) * 2;
+			int hora_criacao = (int)((p[i + 14] & 0xFF) | (p[i + 15] & 0xFF) << 8);
+			int hora = hora_criacao >> 11;
+			int minuto = (hora_criacao >> 5) & 0x3F;
+			int segundo =  (hora_criacao & 0x1F) * 2;
 
 			/* Pegar a data de criação do arquivo */
-			int creationDate = (int)((p[i + 16] & 0xFF) | (p[i + 17] & 0xFF) << 8);
-			int year = 1980 + (creationDate >> 9);
-			int month = (creationDate >> 5) & 0x1F;
-			int day = creationDate & 0x1F;
+			int data_criacao = (int)((p[i + 16] & 0xFF) | (p[i + 17] & 0xFF) << 8);
+			int ano = 1980 + (data_criacao >> 9);
+			int mes = (data_criacao >> 5) & 0x1F;
+			int dia = data_criacao & 0x1F;
 
 			/* Printar os atributos do arquivo do diretório raiz */
 			printf("Tipo de arquivo: %c\n ", ds.extensao);
 			printf("Tamanho do arquivo: %d\n ", ds.tamanho);
 			printf("Nome do arquivo: %s \n", ds.nome_do_arquivo);
-			printf("Data/Hora: %d/%02d/%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second);
+			printf("Data/Hora: %d/%02d/%02d %02d:%02d:%02d\n", ano, mes, dia, hora, minuto, segundo);
 		}
 	}
 

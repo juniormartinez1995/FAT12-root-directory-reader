5df5fb956e5034feaf9e1b1e3392cb77c092390b
Alfonso Martinez <34066160+juniormartinez1995@users.noreply.github.com>
2018-12-08 19:13:56 -0300

    Few updates

--- FAT12reader.c
+++ FAT12reader.c
@@ -6,7 +6,62 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-//#include "diskutils.h"
+
+struct {
+
+    unsigned char salto[3];
+
+    unsigned char nome_oem[8];
+
+    unsigned char bytes_por_setor[2];
+
+    unsigned char setores_por_cluster[1];
+
+    unsigned char setores_reservados[2];
+
+    unsigned char copias_da_fat[1];
+
+    unsigned char entradas_diretorio_raiz[2];
+
+    unsigned char numero_de_setores[2];
+
+    unsigned char descritor_de_midia[1];
+
+    unsigned char setores_por_copia_da_fat[2];
+
+    unsigned char setores_por_trilha[2];
+
+    unsigned char numero_de_cabecas[2];
+
+    unsigned char setores_ocultos[2];
+
+    unsigned char codigo_de_boot[480];
+
+    unsigned char assinatura_de_boot[2];
+
+} BOOT;
+
+
+
+struct {
+
+    char *nome_do_arquivo;
+
+    char extensao;
+
+    int atributos;
+
+    char reservado[10];
+
+    char hora[2];
+
+    char data[2];
+
+    char primeiro_cluster[2];
+
+    int tamanho;
+
+} DESCRITOR;
 
 
 //removeSpaces
@@ -90,7 +145,7 @@ char* nome_arq(char* a, int i)
 
 int main(int argc, char* argv[])
 {
-
+	DESCRITOR ds;
     if (argc != 2)
     {
         printf("E necessario passar o arquivo imagem como argumento\n");
@@ -123,13 +178,13 @@ int main(int argc, char* argv[])
 	/* Loop para printar todos os arquivos do diretório raiz. */
 	for (int i = rootAddress; i < rootAddress + rootSize; i += 32)
 	{
-		int attribute = atributos_dr(p, i);
-		if (attribute != -1 && (int)p[i] != 0)
+		int ds.atributos = atributos_dr(p, i); //attribute = ds.atributo
+		if (ds.atributos != -1 && (int)p[i] != 0)
 		{
 			/* Pegar o tipo, tamanho do arquivo, e o seu nome*/
-			char type = attribute == 0 ? 'D' : 'F';
-			int fileSize = tamanho_arq(p, i);
-			char* fileName = nome_arq(p, i);
+			ds.extensao = ds.atributos == 0 ? 'D' : 'F';
+			ds.tamanho = tamanho_arq(p, i);
+			ds.nome_do_arquivo = nome_arq(p, i);
 
 			/* Pegar a hora de criação do arquivo */
 			int creationTime = (int)((p[i + 14] & 0xFF) | (p[i + 15] & 0xFF) << 8);
@@ -144,9 +199,9 @@ int main(int argc, char* argv[])
 			int day = creationDate & 0x1F;
 
 			/* Printar os atributos do arquivo do diretório raiz */
-			printf("Tipo de arquivo: %c\n ", type);
-			printf("Tamanho do arquivo: %d\n ", fileSize);
-			printf("Nome do arquivo: %s \n", fileName);
+			printf("Tipo de arquivo: %c\n ", ds.extensao);
+			printf("Tamanho do arquivo: %d\n ", ds.tamanho);
+			printf("Nome do arquivo: %s \n", ds.nome_do_arquivo);
 			printf("Data/Hora: %d/%02d/%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second);
 		}
 	}
